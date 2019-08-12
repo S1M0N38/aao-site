2897e11bcfff092989913d7c2546d9c09fb12fc3
S1M0N38 <bertolottosimone@gmail.com>
2019-08-12 12:55:04 +0200

    now DEBUG setting must be declared in the specific config file

--- aao_site/config/settings/production.py
+++ aao_site/config/settings/production.py
@@ -2,8 +2,8 @@ from .base import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = env_var('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS += get_env_variable('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS += env_var('ALLOWED_HOSTS').split(',')
